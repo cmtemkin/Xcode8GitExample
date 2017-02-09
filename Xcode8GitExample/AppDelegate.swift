b0ac42ee811ae502adbd0fd696ae977f85cedf06
Temkin, Charlie <ctemkin@mac-ctemkin.local>
2017-02-09 14:13:18 -0500

    Initial Commit

---
+++ Xcode8GitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8GitExample
+//
+//  Created by Temkin, Charlie on 2/9/17.
+//  Copyright © 2017 Temkin, Charlie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
